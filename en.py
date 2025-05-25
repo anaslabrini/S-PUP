602db309e65506dc55cb001f9fa2df8d1ecc768b
anas labrini <anaslabrini12@gmail.com>
2025-05-25 14:18:18 +0200

    Update en.py

--- en.py
+++ en.py
@@ -14,49 +14,57 @@ import os
 import subprocess
 from hashlib import sha256
 import platform
+
+royal_purple = "\033[1;38;2;128;0;128m" 
+royal_blue = "\033[1;34m"
+red_bold="\033[1;31m"
+crimson = "\033[1;38;2;220;20;60m"          
+turquoise = "\033[1;38;2;64;224;208m"
+blue_bold = "\033[1;38;2;0;0;139m"
+reset = "\033[0m"
 def clear_screen():
     if os.name == "posix":
         os.system("clear")
     elif os.name == "nt":
         os.system("cls")
 clear_screen()
-print("                     From nothing we make everything")
-print("""\033[91m			       vdfji\033[0m#*$pp
-\033[91m	                      m516$9#\033[0m#$$8=m
-\033[91m	                    +88$9WWW$\033[0mWWW#8$$$
-\033[91m	                   8$9$W$9$$9\033[0mW$WWW$WW9W,
-\033[91m	                  799999W$WW$\033[0m$$WWW$$#W$WW
-\033[91m	                 9769$$99W$$W\033[0m$$9@#W#WW#W98
-\033[91m	                .46899$9$W$W9\033[0mW#W##WWW$WWW8=
-\033[91m	               ..57799W$9$9W#\033[0mWWW$#$WW$#9$96.
-\033[91m	               ..59$899WW$W$$\033[0m#8##$W$9$W$$999
-\033[91m	               36545898$9WW9$\033[0m$W9$WW9WW$$97696
-\033[91m	              .334779$WW$$$9W\033[0m$W$$W$W$$W$$9490
-\033[91m	               852889999$8W$9\033[0m$$W#W$$WW$$$94$
-\033[91m	                7648899$#WW$9\033[0m9$W9W$$W$9$858,
-\033[91m	                 41689$$$$9$$\033[0mW9$$$$$$$W978=
-\033[91m	                  .57$W8$9$$9\033[0mW$$WWWW$998=
-\033[91m	                   17$W9$$$$W\033[0m9$W$$9$W998
-\033[91m	                    96799W9W$\033[0m9$WW9W8$88
-\033[91m	                     47799W9#\033[0m$WW$WW$92
-\033[91m	                Anas 45?68$7W\033[0m$9$999488 Labrini
-\033[91m	                     57646658\033[0m8$9877999
-\033[91m	                    ;46875787\033[0m777$9$W98+.
-\033[91m	                  .4678777785\033[0m798$87WW$$7.
-\033[91m	              .-9988798778869\033[0m99$9$$9$$8$W98;.
-\033[91m	          .688$9889$989788999\033[0m98999$$$$$$$$W88899,.
-\033[91m	      ,8789999999$9$8989$89W9\033[0m899$9$8$9$$$$$W$$$89WW9;
-\033[91m	     77$999778$8899W8W$W$999W\033[0m9$#$9WW9W99WWW99$9$9$W9W76
-\033[91m	    788888898$999989789888899\033[0m8$$WW9$99$99$$$$$9$9$W$W98=
-\033[91m	   6778898889$$998888W$$8899W\033[0m9#$$$W#WW$WW$$W$$$$$$$$#$88.
-\033[91m           __       \033[0m__          __  
-\033[91m	  (    __  /__\033[0m)  /  /  /__) 
-\033[91m	 __)      /     \033[0m(__/  /                          
 
-\033[91m      SHARK PUP Tool by Anas Labrini - v1.7 
-\033[0m
+print(f"""{royal_blue}                                                              
+                                                                                                    
+                                        .^^::..                                                    
+                                        :77777!!^:.                                                
+                                          .!!!!!7777!^. 
+                                           :7!!!!!!!777!:                                           
+                                           .7!!!!!!!!!!77!^^^^::...                                
+            :~^:.                         .~7!!!!!!!!!!!!7777777777!!!~^:..                         
+             .!77!^.                  .:^!77!!!!!!!!!!!!!!!!!!!!!!!!7777777!!~:..                  
+               ~7777!:           .:^~!7777!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!77777!^:.               
+                ^77!77!:    ..^~!77777!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!7777!^.          
+                 ^777777!!!!77777!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!777!.        
+                  !777777777!!!!!!!!!{crimson}From nothing we make everything{reset}{royal_blue}!!!!!!!!!!!!!!!!!!!777^{reset}           
+                  ^77777777777!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!77!^.            
+                  !777777!~~~!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!777!~:               
+                 :77777!:      .:~7!77777777!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!7777!!^.                 
+                .7777!^             !~::^~!!77777777!!!!!!!!7!77777777777!!~^.                    
+               .77!~:                       ...::^^!7!!!!!!!7!~~~~~~^^:..                          
+              .!~:                                .!!!!!!77!^                                     
+                                                 :77!!777!:                                        
+                                                ^77777!^.                                          
+                                              .!77!~:.                                             
+                                              ::..                                                 
+                                                                                                    
+                                                             {reset}                                       
+{royal_blue}           __       {reset}__          __  
+{royal_blue}	  (    __  /__{reset})  /  /  /__) 
+{royal_blue}	 __)      /     {reset}(__/  /                          
+
+{royal_blue}      SHARK PUP Tool by Anas Labrini - v1.7{reset}
+{royal_blue}      WebSite:{reset}{crimson}  www.anaslabrini.netlify.app{reset}
+{royal_blue}      Github:{reset}{crimson}   https://github.com/anaslabrini{reset}
+{royal_blue}      Instagram:{reset}{crimson}anasans005{reset}
 """)
-file_path = input("Enter path your script : ")
+
+file_path = input(f"{royal_blue} Enter path your script :{reset} ")
 # اسم السكربت المشفر
 output_file = "S-PUP_AES.py"
 def pad(data):
@@ -105,20 +113,20 @@ enc_code = """{encrypted_code}"""
 password = generate_dynamic_key()
 exec(compile(decrypt(enc_code, password), "<string>", "exec"))
 ''')
-print(f"\n An encrypted script has been created: {output_file}")
+print(f"\n{royal_blue} An encrypted script has been created:{reset} {crimson}{output_file}{reset}")
 # اسأل المستخدم إذا كان يريد تحويله إلى EXE
-to_exe = input(" Do you want to convert the file to .exe? (y/n): ").lower()
+to_exe = input(f"\n{royal_blue} Do you want to convert the file to .exe? (y/n):{reset} ").lower()
 if to_exe == 'y':
     try:
-        new_name = input(" Enter name for your final EXE file (without .py): ").strip()
+        new_name = input(f"\n{royal_blue} Enter name for your final EXE file (without .py):{reset} ").strip()
         if not new_name.endswith(".py"):
             new_name += ".py"
     # إعادة تسمية الملف المشفر إلى الاسم الجديد
         os.rename(output_file, new_name)
-        print(" Converting to EXE using PyInstaller...")
+        print(f"\n{crimson} Converting to EXE using PyInstaller...{reset}")
         subprocess.run(["pyinstaller", "--noconfirm", "--onefile", "--noconsole", new_name])
-        print("\n The executable file was created successfully in the dist/ directory.")
+        print(f"\n{crimson} The executable file was created successfully in the dist/ directory.{reset}")
     except Exception as e:
-        print(f" Error converting to exe: {e}")
+        print(f"\n{red_bold} Error converting to exe:{reset} {crimson}{e}{reset}")
 else:
-    print(" The conversion to .exe has been cancelled.")
+    print(f"\n{crimson} The conversion to .exe has been cancelled.{reset}")
